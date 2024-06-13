8a3716c8399934ecee3e02146c7e8766bace2043
Lucas Pamplona <lucas250598@gmail.com>
2024-06-13 19:00:05 -0300

    Melhor organização

--- src/app/all/page.tsx
+++ src/app/all/page.tsx
@@ -10,10 +10,7 @@ import styles from './styles.module.scss';
 const MAX_PILOTS_PER_RACE = 10;
 const MAX_PILOTS_PER_RACE_MINOR = 6;
 
-interface IRace {
-    pilots: IPilot[];
-    isMinor: boolean;
-}
+type IRace = IPilot[];
 
 const Pilots = () => {
     const { data: pilots } = useQuery<IPilot[]>({
@@ -24,54 +21,39 @@ const Pilots = () => {
         },
     });
 
-    const races: IRace[] = useMemo(() => {
+    const confirmedRaces: IRace[] = useMemo(() => {
         if (!pilots) {
             return [];
         }
 
-        const minors = pilots.filter((pilot) => 18 > pilot.age);
-        const majors = pilots.filter((pilot) => 18 <= pilot.age);
+        const racingPilots = pilots.filter((pilot) => pilot.confirmed && pilot.rating > 0);
 
-        const minorsRacesQuantity = Math.ceil(minors.length / MAX_PILOTS_PER_RACE_MINOR);
-        const majorsRacesQuantity = Math.ceil(majors.length / MAX_PILOTS_PER_RACE);
+        const majorsRacesQuantity = Math.ceil(racingPilots.length / MAX_PILOTS_PER_RACE);
 
-        const minorsPilotsPerRace = Math.ceil(minors.length / minorsRacesQuantity);
-        const majorsPilotsPerRace = Math.ceil(majors.length / majorsRacesQuantity);
+        const majorsPilotsPerRace = Math.ceil(racingPilots.length / majorsRacesQuantity);
 
-        const minorsOrganizedByRating = minors.sort((a, b) => b.rating - a.rating);
-        const majorsOrganizedByRating = majors.sort((a, b) => b.rating - a.rating);
+        const majorsOrganizedByRating = racingPilots.sort((a, b) => b.rating - a.rating);
 
-        const minorsRaces = Array.from({ length: minorsRacesQuantity }, (_, index) => {
-            const start = index * minorsPilotsPerRace;
-            const end = start + minorsPilotsPerRace;
-            return minorsOrganizedByRating.slice(start, end);
-        });
-
-        const majorsRaces = Array.from({ length: majorsRacesQuantity }, (_, index) => {
+        return Array.from({ length: majorsRacesQuantity }, (_, index) => {
             const start = index * majorsPilotsPerRace;
             const end = start + majorsPilotsPerRace;
             return majorsOrganizedByRating.slice(start, end);
         });
-
-        return [
-            ...majorsRaces.map((pilots) => ({ pilots, isMinor: false })),
-            ...minorsRaces.map((pilots) => ({ pilots, isMinor: true })),
-        ];
     }, [pilots]);
+
+    const unconfimedPilots = (pilots || []).filter((pilot) => !pilot.confirmed && pilot.rating > 0);
+    const notRacingPilots = (pilots || []).filter((pilot) => pilot.rating === 0);
 
     return (
         <div className={styles.pilots}>
-            {races.map(({ pilots }, index) => (
+            {confirmedRaces.map((pilots, index) => (
                 <table key={index}>
                     <summary>
                         {'Corrida '}
                         {index + 1}
-                        <small>
-                            {' - '}
-                            {18 <= pilots[0].age ? 'Maior' : 'Menor'}
-                            {' '}
-                            {'de idade'}
-                        </small>
+                        {' - '}
+                        {pilots.length}
+                        {' pilotos'}
                     </summary>
                     <tbody>
                         {pilots.map((pilot) => (
@@ -118,6 +100,104 @@ const Pilots = () => {
                     </tbody>
                 </table>
             ))}
+
+            <table>
+                <summary>
+                    {'Pilotos não confirmados'}
+                </summary>
+                <tbody>
+                    {unconfimedPilots.map((pilot) => (
+                        <tr key={pilot._id}>
+                            <td>
+                                {pilot.firstName}
+                                {' '}
+                                {'"'}
+                                {pilot.nickName}
+                                {'"'}
+                            </td>
+                            <td>
+                                {0 !== pilot.rating
+                                    ? (
+                                        <>
+                                            {pilot.age}
+                                            {' anos'}
+                                        </>
+                                    )
+                                    : 'Não informado'}
+                            </td>
+                            <td>
+                                {pilot.confirmed ? 'Confirmado' : 'Não confirmado'}
+                            </td>
+                            <td>
+                                {pilot.paidAmount.toLocaleString('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL',
+                                })}
+                            </td>
+                            <td>
+                                {0 !== pilot.rating
+                                    ? (
+                                        <>
+                                            {pilot.rating * 50}
+                                            {'cc'}
+                                        </>
+                                    )
+                                    : 'Não vai correr'
+                                }
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            <table>
+                <summary>
+                    {'Pilotos que não vão correr'}
+                </summary>
+                <tbody>
+                    {notRacingPilots.map((pilot) => (
+                        <tr key={pilot._id}>
+                            <td>
+                                {pilot.firstName}
+                                {' '}
+                                {'"'}
+                                {pilot.nickName}
+                                {'"'}
+                            </td>
+                            <td>
+                                {0 !== pilot.rating
+                                    ? (
+                                        <>
+                                            {pilot.age}
+                                            {' anos'}
+                                        </>
+                                    )
+                                    : 'Não informado'}
+                            </td>
+                            <td>
+                                {pilot.confirmed ? 'Confirmado' : 'Não confirmado'}
+                            </td>
+                            <td>
+                                {pilot.paidAmount.toLocaleString('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL',
+                                })}
+                            </td>
+                            <td>
+                                {0 !== pilot.rating
+                                    ? (
+                                        <>
+                                            {pilot.rating * 50}
+                                            {'cc'}
+                                        </>
+                                    )
+                                    : 'Não vai correr'
+                                }
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 };
